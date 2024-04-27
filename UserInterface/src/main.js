86377425fdbc4e96b60c9e6960d8fe903963fa38
Mark Bell <me@markb.co.uk>
2024-04-27 13:21:07 +0100

    Add missing worker definition for Monaco editor

--- UserInterface/src/main.js
+++ UserInterface/src/main.js
@@ -1,16 +1,24 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
-import Wind from './presets/wind'; 
+import Wind from './presets/wind';
 
 import App from './App.vue';
 
 import './assets/main.css';
 
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+
+self.MonacoEnvironment = {
+    getWorker(_, label) {
+        return new editorWorker();
+    },
+};
+
 const app = createApp(App);
 
 app.use(PrimeVue, {
     unstyled: true,
-    pt: Wind
+    pt: Wind,
 });
 
 app.mount('#app');
